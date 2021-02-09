368a8084cc83bbc1369a49a23b2c8dd6a0039635
Rastko <rastkodjurica@yahoo.com>
2021-02-09 00:28:43 -0500

    RastkoReact

---
+++ src/Components/IdCard.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const IdCard = (props) => {
+    return (
+        <div className="idCard-box">
+            <div>
+                <img src={props.picture} alt=""></img>
+            </div>
+            <div>
+                Fist Name: {props.firstName}
+            </div>
+            <div>
+                Last name: {props.lastName}
+            </div>
+            <div>
+                Gender: {props.gender}
+            </div>
+            <div>
+                Height: {props.height}
+            </div>
+            <div>
+                Birthday: {moment(props.birth).format('LLLL')}
+            </div>
+
+        </div>
+    );
+};
+
+export default IdCard;
